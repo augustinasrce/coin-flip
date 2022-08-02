e7d8f7dad8d86421a1b7f17d4ba1689a93be71df
Augustinas Sueris <augustinas.sueris@vinted.com>
2022-08-02 22:18:32 +0300

    installed prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import './App.css';
-import { useState } from 'react';
+import './App.css'
+import { useState } from 'react'
 import { CoinSide, getRandomCoinSide, imgConfig } from './CoinSide'
 
 const App = (): JSX.Element => {
@@ -9,9 +9,11 @@ const App = (): JSX.Element => {
   const [tailsCount, setTailsCount] = useState<number>(0)
 
   const handleFlip = (): void => {
-    setCoinSide(getRandomCoinSide());
-    setFlipCount(flipCount + 1);
-    coinSide === CoinSide.HEAD ? setHeadsCount(headsCount + 1) : setTailsCount(tailsCount + 1);
+    setCoinSide(getRandomCoinSide())
+    setFlipCount(flipCount + 1)
+    coinSide === CoinSide.HEAD
+      ? setHeadsCount(headsCount + 1)
+      : setTailsCount(tailsCount + 1)
   }
 
   return (
@@ -19,17 +21,25 @@ const App = (): JSX.Element => {
       <div className="text-container">
         <p>Flipper app</p>
       </div>
-      <div className='img-container'>
-        <input type="image" onClick={handleFlip} src={imgConfig[coinSide].url} alt={imgConfig[coinSide].alt} />
+      <div className="img-container">
+        <input
+          type="image"
+          onClick={handleFlip}
+          src={imgConfig[coinSide].url}
+          alt={imgConfig[coinSide].alt}
+        />
       </div>
       <div className="text-container">
         <p>Fliped {flipCount} times</p>
         {flipCount ? (
-          <p className="small">Heads {headsCount} ({(headsCount / flipCount * 100).toFixed(2)}%), Tails {tailsCount} ({(tailsCount / flipCount * 100).toFixed(2)}%)</p>
+          <p className="small">
+            Heads {headsCount} ({((headsCount / flipCount) * 100).toFixed(2)}%),
+            Tails {tailsCount} ({((tailsCount / flipCount) * 100).toFixed(2)}%)
+          </p>
         ) : null}
       </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/CoinSide.tsx
+++ src/CoinSide.tsx
@@ -3,18 +3,18 @@ import heads from './assets/heads.png'
 import unknown from './assets/repeat.svg'
 
 export enum CoinSide {
-    HEAD,
-    TAIL,
-    UNKNOWN
+  HEAD,
+  TAIL,
+  UNKNOWN,
 }
 
 export const getRandomCoinSide = (): CoinSide => {
-    const key: number = Math.round(Math.random());
-    return key ? CoinSide.HEAD : CoinSide.TAIL;
+  const key: number = Math.round(Math.random())
+  return key ? CoinSide.HEAD : CoinSide.TAIL
 }
 
 export const imgConfig = {
-    0: { url: heads, alt: 'Heads' },
-    1: { url: tails, alt: 'Tails' },
-    2: { url: unknown, alt: 'Flip me!' }
+  0: { url: heads, alt: 'Heads' },
+  1: { url: tails, alt: 'Tails' },
+  2: { url: unknown, alt: 'Flip me!' },
 }
